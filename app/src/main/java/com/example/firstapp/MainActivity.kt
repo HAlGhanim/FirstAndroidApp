b7012b2e6c54259da6799480511f7d33b64b50ba
HAlGhanim <imzaji98@gmail.com>
2025-05-20 13:40:31 +0300

    initial commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstapp.ui.theme.FirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirstAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                     Text(text= "Android is an operating system.",style = MaterialTheme.typography.headlineLarge)
+                    }
+                    Row {
+                        Button(onClick = {},modifier = Modifier.background(Color.Blue)) {
+                            Text(text = "True")
+                        }
+                        Button(onClick = {},modifier = Modifier.background(Color.Blue)) {
+                            Text(text = "False")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
